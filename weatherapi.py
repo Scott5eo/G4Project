ded80db8cb6768520bd13031026736544a8e66c2
Scott5eo <32429179+Scott5eo@users.noreply.github.com>
2018-05-21 18:17:09 +0400

    Update weatherapi.py

--- weatherapi.py
+++ weatherapi.py
@@ -6,30 +6,30 @@ Created on Sat May 19 19:26:40 2018
 """
 from weather import Weather, Unit
 
-class locationWeather(object):
-    def __init__(self, location):
+class locationWeather(object): #This is a weather-api class that uses Weather-api, a yahoo weather api wrapper.
+    def __init__(self, location): #this loads the initial weather of the location given
         self.location = location
         self.weather = Weather(unit=Unit.CELSIUS).lookup_by_location(self.location)
         self.condition = self.weather.condition
     
     def getForecasts(self):
-        return self.weather.forecast #returns 5 day forecast objects
+        return self.weather.forecast #this returns 5 day forecast objects which to be used later
     
     def getWeather(self):
-        return self.weather #returns weather object
+        return self.weather #returns weather object which contains the day's weather
     
     def getWind(self):
-        return self.weather.wind.speed
+        return self.weather.wind.speed #Returns wind speed
     
     def getForecastHighs(self):
         dateHighs = {}
         for forecast in self.getForecasts():
             dateHighs[forecast.date] = forecast.high
-        return dateHighs #returns a dictionary
+        return dateHighs #returns a dictionary of highs to keys of days
     
     def getForecastDates(self):
         dates = []
         for forecast in self.getForecasts():
             dates.append(forecast.date)
-        return dates #return a list 
+        return dates #return a list of days in order of forecast to be used with evaporation estimation in another class.
     
